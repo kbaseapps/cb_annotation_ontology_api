442e23d9c3312e2c086efaca38dd0f922495d795
Christopher Henry <chenry@mcs.anl.gov>
2022-09-15 23:43:01 -0500

    Fixing bug in save genome function call

--- lib/cb_annotation_ontology_api/annotation_ontology_api.py
+++ lib/cb_annotation_ontology_api/annotation_ontology_api.py
@@ -525,8 +525,8 @@ class AnnotationOntologyAPI:
             else:
                 gfu_param['workspace'] = params["output_workspace"]
             save_output = self.gfu_client.save_one_genome(gfu_param);
-            output["output_ref"] = str(save_output[0][6])+"/"+str(save_output[0][0])+"/"+str(save_output[0][4])
-            output["output_name"] = str(save_output[0][1])
+            output["output_ref"] = str(save_output[6])+"/"+str(save_output[0])+"/"+str(save_output[4])
+            output["output_name"] = str(save_output[1])
         else:            
             #Returning object if save not requested
             output["object"] = params["object"]
